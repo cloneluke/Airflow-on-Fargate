ba4aff3ecc96a3f0dbbfe44a8db11f981a4b4bac
Luke Bodeen <lbodeen@docutap.com>
2021-09-29 11:00:53 -0500

    fixed line feeds in entry scripts tested airflow upgrade upgraded to pscopg2

--- airflow/config/webserver_entry.sh
+++ airflow/config/webserver_entry.sh
@@ -2,10 +2,11 @@
 
 set -Eeuxo pipefail
 
-airflow initdb
+airflow db init
 sleep 5
 
-airflow create_user -r Admin -u admin -f FirstName -l LastName -p ${ADMIN_PASS} -e admin@test.com
+#airflow create_user -r Admin -u admin -f FirstName -l LastName -p ${ADMIN_PASS} -e admin@test.com
+airflow users create --role Admin --username admin --firstname FirstName --lastname LastName --password ${ADMIN_PASS} --email admin@test.com
 sleep 5
 
 airflow webserver
